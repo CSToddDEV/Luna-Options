66b33b4647fac0591210e45d7c722a647d99f679
Calvin Todd <CSTodd88@gmail.com>
2021-05-14 17:38:48 -0700

    checks table len

--- Luna Options Server/ServerUpdate.py
+++ Luna Options Server/ServerUpdate.py
@@ -6,33 +6,33 @@ import LunaOptionsDB as db
 import SandP500List as snp
 
 
-def update():
-    luna = db.LunaDB()
-    api_obj = api.APICalls()
-    for ticker in snp.ticker_list:
-        ticker.lower()
-        columns = luna.get_column_names(ticker, '_options')
-        print(columns)
-        print('Ticker ', ticker)
-        if len(columns) == 5:
-            print('ADDING COLUMN')
-            luna.add_column(ticker, '_options', 'historicalVolatility', 'varchar(32)')
+# def update5_14_21():
+#     luna = db.LunaDB()
+#     api_obj = api.APICalls()
+#     for ticker in snp.ticker_list:
+#         ticker.lower()
+#         columns = luna.get_column_names(ticker, '_options')
+#         print(columns)
+#         print('Ticker ', ticker)
+#         if len(columns) == 5:
+#             print('ADDING COLUMN')
+#             luna.add_column(ticker, '_options', 'historicalVolatility', 'varchar(32)')
+#
+#         hv = api_obj.historical_volatility_call(ticker)
+#         if hv and 'indicator' in hv.keys():
+#             historical_volatility = hv['indicator'][0][0]
+#             historical_volatility = float(historical_volatility)
+#             historical_volatility = historical_volatility * 100
+#             historical_volatility = round(historical_volatility, 2)
+#
+#         else:
+#             historical_volatility = 'N/A'
+#         try:
+#             luna.update_column_conditional(ticker, '_options', 'historicalVolatility', str(historical_volatility), 'id', '1')
+#         except:
+#             print('PASSED: ', ticker)
+#
+#     print('Update Complete!')
 
-        hv = api_obj.historical_volatility_call(ticker)
-        if hv and 'indicator' in hv.keys():
-            historical_volatility = hv['indicator'][0][0]
-            historical_volatility = float(historical_volatility)
-            historical_volatility = historical_volatility * 100
-            historical_volatility = round(historical_volatility, 2)
-
-        else:
-            historical_volatility = 'N/A'
-        try:
-            luna.update_column_conditional(ticker, '_options', 'historicalVolatility', str(historical_volatility), 'id', '1')
-        except:
-            print('PASSED: ', ticker)
-
-    print('Update Complete!')
 
 
-update()
