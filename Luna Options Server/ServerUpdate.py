85ad32ca5788a5662178519ecabbb751743d8f8a
Calvin Todd <CSTodd88@gmail.com>
2021-05-17 23:56:20 -0700

    get top 50 test

--- Luna Options Server/ServerUpdate.py
+++ Luna Options Server/ServerUpdate.py
@@ -127,7 +127,3 @@ from LunaOptionsDBUpdate import DBUpdate as update
 #     luna.update_high_iv_table()
 #
 #     print("Server Update Finished!")
-
-luna = db.LunaDB()
-luna.update_high_iv_table()
-print(luna.get_top_50())
